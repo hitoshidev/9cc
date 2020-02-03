e4ed4df5b5792d43ea5a8c65bde7fedac4f7469a
渡辺 仁志 <watanabe.h@wingarc.com>
2020-02-03 13:05:19 +0900

    四則演算を追加

--- src/9cc.c
+++ src/9cc.c
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+char* user_input;
+
 typedef enum {
   TK_RESERVED,
   TK_NUM,
@@ -12,19 +14,30 @@ typedef enum {
 } TokenKind;
 
 typedef struct Token Token;
-
 struct Token {
   TokenKind kind;
   Token* next;
   int val;
   char* str;
 };
-
 Token *token;
 
 void error(char* fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+void error_at(char* loc, char* fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, "");
+  fprintf(stderr, "^ ");
   vfprintf(stderr, fmt, ap);
   fprintf(stderr, "\n");
   exit(1);
@@ -40,13 +53,13 @@ bool consume(char op) {
 
 void expect(char op) {
   if (token->kind != TK_RESERVED || token->str[0] != op)
-    error("'%c'ではありません", op);
+    error_at(token->str, "'%c'ではありません", op);
   token = token->next;
 }
 
 int expect_number() {
   if (token->kind != TK_NUM)
-    error("数ではありません");
+    error_at(token->str, "数ではありません");
   int val = token->val;
   token = token->next;
   return val;
@@ -76,7 +89,7 @@ Token *tokenize(char *p) {
       continue;
     }
 
-    if (*p == '+' || *p == '-') {
+    if (strchr("+-*/()", *p)) {
       cur = new_token(TK_RESERVED, cur, p++);
       continue;
     }
@@ -87,11 +100,114 @@ Token *tokenize(char *p) {
       continue;
     }
 
-    error("トークナイズできません");
+    error_at(token->str, "トークナイズできません");
   }
 
   new_token(TK_EOF, cur, p);
   return head.next;
+}
+
+typedef enum {
+  ND_ADD,
+  ND_SUB,
+  ND_MUL,
+  ND_DIV,
+  ND_NUM
+} NodeKind;
+
+typedef struct Node Node;
+struct Node {
+  NodeKind kind;
+  Node* lhs;
+  Node* rhs;
+  int val;
+};
+
+Node* new_node_num(int val) {
+  Node* node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+Node* new_node(NodeKind kind, Node* lhs, Node* rhs) {
+  Node* node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node* expr();
+
+Node* primary() {
+  if(consume('(')) {
+    Node* node = expr();
+    expect(')');
+    return node;
+  }
+  return new_node_num(expect_number());
+}
+
+Node* mul() {
+  Node* node = primary();
+
+  while(1) {
+    if(consume('*')) {
+      node = new_node(ND_MUL, node, primary());
+    }
+    else if(consume('/')) {
+      node = new_node(ND_DIV, node, primary());
+    }
+    else  {
+      return node;
+    }
+  }
+}
+
+Node* expr() {
+  Node* node = mul();
+
+  while(1) {
+    if(consume('+')) {
+      node = new_node(ND_ADD, node, mul());
+    }
+    else if(consume('-')) {
+      node = new_node(ND_SUB, node, mul());
+    }
+    else {
+      return node;
+    }
+  }
+}
+
+void gen(Node* node) {
+  if(node->kind == ND_NUM) {
+    printf("  push %d\n", node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch(node->kind) {
+    case ND_ADD:
+      printf("  add rax, rdi\n");
+      break;
+    case ND_SUB:
+      printf("  sub rax, rdi\n");
+      break;
+    case ND_MUL:
+      printf("  imul rax, rdi\n");
+      break;
+    case ND_DIV:
+      printf("  cqo\n");
+      printf("  idiv rdi\n");
+  }
+  printf("  push rax\n");
 }
 
 int main(int argc, char** argv) {
@@ -100,23 +216,17 @@ int main(int argc, char** argv) {
     return 1;
   } 
 
-  token = tokenize(argv[1]);
+  user_input = argv[1];
+  token = tokenize(user_input);
+  Node* node = expr();
 
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-  printf("  mov rax, %d\n", expect_number());
 
-  while(!at_eof()) {
-    if(consume('+')) {
-      printf("  add rax, %d\n", expect_number());
-      continue;
-    }
-    if(consume('-')) {
-      printf("  sub rax, %d\n", expect_number());
-      continue;
-    }
-  }
+  gen(node);
+
+  printf("  pop rax\n");
   printf("  ret\n");
   return 0;
 }
